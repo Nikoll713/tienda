ab585fe2720363e2f1bdf993d34f8d35b19cdc0b
Nikoll Bonilla <nikoll.bonilla@tecsup.edu.pe>
2021-05-24 09:01:27 -0500

    Migracion_Lista_compras

--- database/migrations/2021_05_24_134245_create_lista_compras_table.php
+++ database/migrations/2021_05_24_134245_create_lista_compras_table.php
@@ -16,6 +16,11 @@ class CreateListaComprasTable extends Migration
         Schema::create('lista_compras', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('producto_id');
+            $table->unsignedBigInteger('compra_id');
+            $table->integer('cantidad');
+            $table->foreign('producto_id')->references('id')->on('productos');
+            $table->foreign('compra_id')->references('id')->on('compras');
         });
     }
 
